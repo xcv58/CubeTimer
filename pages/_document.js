53180a0f8c08eec344114d1902d8f2172c5b9ce0
xcv58 <i@xcv58.com>
2017-04-08 23:25:51 -0700

    Add offline support

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name='viewport' content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui' />
+          <link rel='manifest' href='static/manifest.json' />
+          <title>Rubik's Cube Timer</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-const dev = process.env.NODE_ENV !== 'production'
+// const dev = process.env.NODE_ENV !== 'production'
+//
+// const { createServer } = require('http')
+// const { parse } = require('url')
+// const next = require('next')
+// const mobxReact = require('mobx-react')
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+//
+// mobxReact.useStaticRendering(true)
+//
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true)
+//     handle(req, res, parsedUrl)
+//   }).listen(3000, err => {
+//     if (err) throw err
+//     console.log('> Ready on http://localhost:3000')
+//   })
+// })
 
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
+const path = require('path')
 const next = require('next')
-const mobxReact = require('mobx-react')
-const app = next({ dev })
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dir: '.', dev })
 const handle = app.getRequestHandler()
 
-mobxReact.useStaticRendering(true)
+app.prepare()
+  .then(_ => {
+    const server = express()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3000, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    // serve service worker
+    server.get('/sw.js', (req, res) => res.sendFile(path.resolve('./.next/sw.js')))
+
+    server.get('*', (req, res) => handle(req, res))
+
+    server.listen(3000, err => {
+      if (err) throw err
+
+      console.log('> App running on port 3000')
+    })
   })
-})
