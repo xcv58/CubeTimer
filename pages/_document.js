06da5792ffe70d634e757f82e87935ac3c426332
xcv58 <i@xcv58.com>
2017-04-10 20:55:59 -0700

    Update backgroud color

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,10 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render () {
     return (
-      <html>
+      <html style={{ background: '#F5F5F5' }}>
         <Head>
           <meta name='viewport' content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui' />
-          <meta name='theme-color' content='#FFF' />
+          <meta name='theme-color' content='#F5F5F5' />
           <link rel='manifest' href='static/manifest.json' />
           <title>Rubik's Cube Timer</title>
         </Head>
